210132039753d6030aaaf30219c9da7e536ba73c
TEgo17 <thamodh.e17@gmail.com>
2023-04-04 14:15:54 -0400

    adding the pom.xml file to Git as well as minor comments to the TestSteps.java file

--- src/test/java/com/qaconsultants/TestSteps.java
+++ src/test/java/com/qaconsultants/TestSteps.java
@@ -12,12 +12,12 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestSteps implements En {
     public TestSteps() {
 
-
-
+       // setting up the configuration browser before each given
        Before(() -> {
            Configuration.browser = "firefox";
            Configuration.browserBinary = "C:/Program Files/Mozilla Firefox/firefox.exe";
            System.setProperty("webdriver.gecko.driver", "C:/Users/tegodawatte/Downloads/geckodriver-v0.32.2-win32/geckodriver.exe");
+       });
 
            /*
            SelenideElement frame = Selenide.$(
@@ -37,68 +37,68 @@ public class TestSteps implements En {
            }
 
             */
-       });
 
-        // lambda expression, functional programming
-        // Given method (Gherkin), built in
+
+        // Given to: open the login page
         Given("^I open login page$", () -> {
             System.out.println("I am on the login page");
 
             open("https://www.automationexercise.com/login");
-
         });
 
+        // Given to: log into the website with valid credentials
         Given("^I login as a registered user$", () -> {
             System.out.println("I input my username and password on the login page");
+
             open("https://www.automationexercise.com/login");
 
             $(By.name("email")).sendKeys("thamodh@email.com");
             $(By.name("password")).sendKeys("Password");
             $x("//button[@type='submit']").click();
-
         });
 
+        // Given to: switch from the home page to the products page
         Given("^I switch to open products page$", () -> {
             System.out.println("I am now on the products page");
 
             open("https://www.automationexercise.com/products");
-
         });
 
+        // Given to: search for 'tshirts' in the search bar of the products page
         Given("^I search t shirts on the products page$", () -> {
             System.out.println("I have now searched for t shirts on the products page");
 
             $x("//input[@id='search_product']").sendKeys("tshirts");
             $x("//button[@id='submit_search']").click();
-
-             sleep(2000);
         });
 
+        // Given to: select two tshirts from the products page and them to the cart
         Given("^I select two shirts I want and add to cart$", () -> {
             System.out.println("I am now selecting two shirts and adding to the cart");
 
-            // first shirt
+            // add first shirt
             $x("//div[@class='product-image-wrapper']").scrollTo().hover();
             $x("//div[@class='single-products']/*/*[@class='btn btn-default add-to-cart']").shouldBe(Condition.enabled, Duration.ofSeconds(3)).click();
 
-            // Continue shopping button
+            // press 'Continue Shopping' button
             $x("//div[@class='modal-content']/*/*[@class='btn btn-success close-modal btn-block']").click();
 
-            // Second shirt
+            // add secord shirt
             $x("//div[@class='product-image-wrapper']").scrollTo().hover();
             $x("//div[@class='single-products']/*/*[@data-product-id='28']").shouldBe(Condition.enabled, Duration.ofSeconds(3)).click();
 
-            // View Cart
+            // press 'View Cart' button
             $x("//*[text()='View Cart']").click();
-
         });
 
+        // Given to: remove a shirt from the cart
         Given("^I remove one shirt I do not want from the cart$", () -> {
             System.out.println("I am now removing one shirts from the cart");
 
             $x("//*[@class='cart_delete']").click();
         });
 
+        // Given to: proceed to checkout and place the order
         Given("^I proceed to checkout to place my order$", () -> {
             System.out.println("I have now proceeded to checkout and I am placing my order");
 
@@ -107,10 +107,9 @@ public class TestSteps implements En {
 
             // Place order button
             $x("//*[text()='Place Order']").scrollTo().click();
-
-
         });
 
+        // Given to: add the payment info to confirm the purchase of the order
         Given("^I add my payment info to confirm my order$", () -> {
             System.out.println("I am now adding my payment info to confirm my order");
 
@@ -121,15 +120,13 @@ public class TestSteps implements En {
             $(By.name("expiry_year")).sendKeys("2030");
 
             $x("//*[@class='form-control btn btn-primary submit-button']").click();
-
         });
 
+        // Given to: download the invoice of the order that was placed
         Given("^I download the invoice for the order I placed$", () -> {
             System.out.println("I am now downloading my invoice for the order I placed");
 
             $x("//*[text()='Download Invoice']").click();
-
-
         });
 
     }
